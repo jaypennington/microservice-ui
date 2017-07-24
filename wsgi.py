72a15bd23c868c6565a4d932c9e27196ff64cc83
jpenn92 <jpenn92@vt.edu>
2017-07-24 16:46:07 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,19 @@
 from flask import Flask
 application = Flask(__name__)
 
+from Tkinter import *
+
 @application.route("/")
 def hello():
+    #create the window
+    root = Tk()
+
+    #modify root window
+    root.title("Simple GUI")
+    root.geometry("200x100")
+
+    #kick off the event loop
+    root.mainloop()
     return "Oh hai Mark!"
 
 if __name__ == "__main__":
