14cdde5408038abd43698eb8b37aec036d482d0d
jpenn92 <jpenn92@vt.edu>
2017-07-24 17:01:18 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,12 +8,3 @@ def hello():
 
 if __name__ == "__main__":
     application.run()
-    #create the window
-    root = Tk()
-
-    #modify root window
-    root.title("Simple GUI")
-    root.geometry("200x100")
-
-    #kick off the event loop
-    root.mainloop()
